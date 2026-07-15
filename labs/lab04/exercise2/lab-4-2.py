594db646f3731db608e9b3dbdb32ca8aa8964baa
nurfarhanabtjamal <nurfarhanabtjamal@gmail.com>
2026-07-15 12:17:41 +0800

    lab2

--- labs/lab04/exercise2/lab-4-2.py
+++ labs/lab04/exercise2/lab-4-2.py
@@ -2,9 +2,9 @@ income = int(input())
 if income <= 50000:
     tax = 0.0
 else:
-    if income > 50000 and income <= 100000:
-        tax = 0.01
-    else:
+    if income >= 100000:
         tax = 0.02
+    else:
+        tax = 0.01
 totalTax = income * tax
 print(totalTax)
