aa07f62454f39e204284b8dbea996fdf751b2f35
nurfarhanabtjamal <nurfarhanabtjamal@gmail.com>
2026-07-15 12:01:08 +0800

    lab1

--- labs/lab04/exercise1/lab-4-1.py
+++ labs/lab04/exercise1/lab-4-1.py
@@ -1,10 +1,9 @@
 kwh = int(input())
-if kwh == 100:
+if kwh <= 100:
     totalBill = kwh * 0.3
 else:
-    if kwh >= 100 and kwh <= 200:
-        totalBill = 100 * 0.3 + kwh - 100 * 0.5
+    if kwh >= 200:
+        totalBill = 100 * 0.3 + kwh - 100 * 0.75
     else:
-        if kwh > 200:
-            totalBill = 100 * 0.3 + kwh - 100 * 0.75
+        totalBill = 100 * 0.3 + kwh - 100 * 0.5
 print(totalBill)
