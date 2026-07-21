a435b2e1a7d927443609a5ba8b3a1575fb12e1f0
nurfarhanabtjamal <nurfarhanabtjamal@gmail.com>
2026-07-22 03:56:14 +0800

    update

--- labs/lab03/exercise3/lab-03-3.py
+++ labs/lab03/exercise3/lab-03-3.py
@@ -1,10 +1,9 @@
 kilobyte = int(input())
-megabyte = float(kilobyte) / 1000
-gigabyte = (kilobyte / 1000 ** 2)
-terabyte = (kilobyte / 1000 ** 3)
-petabyte = (kilobyte / 1000 ** 4)
+megabyte = float(kilobyte) / 1024
+gigabyte = kilobyte / 1024 ** 2
+terabyte = kilobyte / 1024 ** 3
+petabyte = kilobyte / 1024 ** 4
 print(megabyte)
 print(gigabyte)
 print(terabyte)
 print(petabyte)
- 
