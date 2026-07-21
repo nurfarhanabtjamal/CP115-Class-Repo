de4a5cdc638da286d975c0a9c446ed6e0b8ea735
nurfarhanabtjamal <nurfarhanabtjamal@gmail.com>
2026-07-22 03:48:20 +0800

    update

---
+++ labs/lab03/exercise3/lab-03-3.py
@@ -0,0 +1,10 @@
+kilobyte = int(input())
+megabyte = float(kilobyte) / 1000
+gigabyte = kilobyte / 1000 ** 2
+terabyte = kilobyte / 1000 ** 3
+petabyte = kilobyte / 1000 ** 4
+print(megabyte)
+print(gigabyte)
+print(terabyte)
+print(petabyte)
+ 
---
+++ labs/lab03/exercise4/lab-03-4.py
@@ -0,0 +1,3 @@
+pages = int(input())
+totalCost = (0.2 * pages) + 3
+print(totalCost)
