630832cbe376de3ca8479e639f0a99d6ba5f9f1a
nurfarhanabtjamal <nurfarhanabtjamal@gmail.com>
2026-08-05 11:44:54 +0800

    ex3

---
+++ labs/lab05/exercise3.py
@@ -0,0 +1,4 @@
+import random
+class_name = input("Enter your class name: ")
+random_number = random.randint(1, 100)
+print(f"The student's class name is {class_name} and the random numberis {random_number}.")
