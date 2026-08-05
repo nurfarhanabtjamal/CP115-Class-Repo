422f702d7c8b96c9aaf05977a6de1668aad7fec5
nurfarhanabtjamal <nurfarhanabtjamal@gmail.com>
2026-08-05 12:12:29 +0800

    ex5

---
+++ labs/lab05/exercise5.py
@@ -0,0 +1,12 @@
+test_score1 = int(input("Enter the first test score: "))
+test_score2 = int(input("Enter the second test score: "))
+test_score3 = int(input("Enter the third test score: "))
+
+total_score = test_score1 + test_score2 + test_score3
+average_score = total_score / 3
+
+print(f"the first score is {test_score1}")
+print(f"the second score is {test_score2}")
+print(f"the third score is {test_score3}")
+print(f"the total score is {total_score}")
+print(f"the average score is {average_score}")
