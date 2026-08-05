bcedeb3281a1c8e7aeb458153a5e1f1e264ec686
nurfarhanabtjamal <nurfarhanabtjamal@gmail.com>
2026-08-05 11:37:39 +0800

    ex2

---
+++ labs/lab05/exercise2.py
@@ -0,0 +1,5 @@
+import math
+radius = float(input("Enter the radius of the circle: "))
+area = math.pi * radius ** 2
+circumference = 2 * math.pi * radius
+print(f"The area of the circle is {area} and the circumference of the circle is {circumference}.")
